470796f19421b4f1dcc00e3adfafe57312d8ebbe
Maximiliano DUva <techmax@adinet.com.uy>
2025-03-20 15:42:16 -0300

    cambio texto en pagina principal

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aplicacion de reserva de turnos
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://turnosmove.portal21.work"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pagina principal
         </a>
       </header>
     </div>
