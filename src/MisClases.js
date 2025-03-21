2f82c3876379badb3cbf144b2bfa3c797339f0e9
Maximiliano DUva <techmax@adinet.com.uy>
2025-03-21 12:35:07 -0300

    state

--- src/MisClases.js
+++ src/MisClases.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 
-
-const LoginBtn = () => {
+const LoginBtn = (props) => {
     return (
-      <button className="homeBtn">Iniciar Sesion</button>
+      <button onClick={props.onclic} className="homeBtn">Iniciar Sesion</button>
     );
   };
 
-const LogoutBtn = () => {
+const LogoutBtn = (props) => {
     return (
-      <button className="homeBtn">Cerrar Sesion</button>
+      <button onClick={props.onclic} className="homeBtn">Cerrar Sesion</button>
     );
   }
 
@@ -19,29 +18,78 @@ const AdminBtn = () => {
     );
   }
 
+
+
 class MyNavBar extends Component {
     constructor(props) {
-      super(props);
+        super(props);
+
+        this.state = {
+            isLogued: false,
+            isAdmin: false,
+        }
+
+        this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this);
     }
 
+    login() {
+        this.setState({isLogued: true});
+    }
 
+    logout() {
+        this.setState({isLogued: false});
+    }
+       
     
    render() {
-        
         return ( 
           <div className="MyNavBar">
             <AdminBtn />
-            <LoginBtn />
-            <LogoutBtn />  
-            <p className="message" id="message">{this.props.userAgent} - Datos de sesion: {this.props.stateData[0]} - Esta Logued: {this.props.stateData[1] ? "Si" : "No"} -  Es Admin: {this.props.stateData[2] ? "Si" : "No"}</p>
+            <LoginBtn onclic={this.login}/>
+            <LogoutBtn onclic={this.logout} />  
+            <p className="message" id="message">{this.props.userAgent} - Datos de sesion: {this.props.stateData[0]} - Esta Logued: {this.state.isLogued ? "Si" : "No"} -  Es Admin: {this.state.isAdmin ? "Si" : "No"}</p>
           </div>
         );
     }
   };
-
+  
+  /*
   MyNavBar.defaultProps = {
     userAgent: "Desconocido",
     stateData: ["Invitado", false, false]
   };
+  NO FUNCIONA
+  */
 
+  /*
+  MyNavBar.propTypes = {
+    userAgent: React.PropTypes.string,
+    stateData: React.PropTypes.array
+  };
+  NO FUNCIONA
+  */
+/*
+switch (this.props.stateData[1]) {
+    case true:
+        this.setState({isLogued: true});
+        break;
+    case false: 
+        this.setState({isLogued: false});
+        break;
+    default:
+        break;
+}
+
+switch (this.props.stateData[2]) {
+    case true:
+        this.setState({isAdmin: true});
+        break;
+    case false: 
+        this.setState({isAdmin: false});
+        break;
+    default:
+        break;
+}
+*/
   export default MyNavBar;
